f3d2e9fc0cbac4cf66e6c5abbdee4b117889409d
Dan Snow <dan@sno.ws>
2024-10-06 16:55:34 -0400

    Create seperate buffer param for network and dests

--- data/src/create_destpoint.py
+++ data/src/create_destpoint.py
@@ -3,10 +3,12 @@ from pathlib import Path
 
 import geopandas as gpd
 import pandas as pd
-from utils.census import points_to_gdf
+from utils.census import load_shapefile, points_to_gdf
 
 
-def create_destpoint(year: str, geography: str, state: str) -> None:
+def create_destpoint(
+    year: str, geography: str, state: str, buffer: int = 0
+) -> None:
     """
     Find all centroids (weighted and unweighted) of a given Census geography
     within a buffered version of the containing state.
@@ -14,6 +16,8 @@ def create_destpoint(year: str, geography: str, state: str) -> None:
     :param year: The year of the point data.
     :param geography: The geography type of the point data.
     :param state: The two-digit state code for the point data.
+    :param buffer: The amount to buffer the input shapefile (in meters) when
+        when determining destination points.
     """
     cenloc_dir = (
         Path.cwd()
@@ -22,14 +26,13 @@ def create_destpoint(year: str, geography: str, state: str) -> None:
         / f"year={year}"
         / f"geography={geography}/"
     )
-    osmclip_file = (
+    tiger_file = (
         Path.cwd()
-        / "intermediate"
-        / "osmclip"
+        / "input"
+        / "tiger"
         / f"year={year}"
         / "geography=state"
-        / f"state={state}"
-        / f"{state}.geojson"
+        / "state.zip"
     )
     output_dir = (
         Path.cwd()
@@ -43,19 +46,23 @@ def create_destpoint(year: str, geography: str, state: str) -> None:
     output_dir.mkdir(parents=True, exist_ok=True)
 
     # Load the buffered state boundary
-    osmclip = gpd.read_file(osmclip_file)
-    osmclip.to_crs("EPSG:5071", inplace=True)
+    boundary = load_shapefile(tiger_file)
+    boundary = boundary[boundary["geoid"] == state]
+    boundary = boundary[["geometry"]]
+    boundary.to_crs(crs="EPSG:5071", inplace=True)
+    if buffer:
+        boundary["geometry"] = boundary["geometry"].buffer(distance=buffer)
 
     # Load the Census geography centroids (weighted and unweighted), and keep
     # only centroids of either kind that are within the buffered state
     cenloc = pd.read_parquet(cenloc_dir)
     cenloc_gdf = points_to_gdf(cenloc, "x_5071", "y_5071", "EPSG:5071")
-    cenloc_gdf = cenloc_gdf.sjoin(osmclip, how="inner", predicate="within")
+    cenloc_gdf = cenloc_gdf.sjoin(boundary, how="inner", predicate="within")
     cenloc_gdf_wt = points_to_gdf(
         cenloc, "x_5071_wt", "y_5071_wt", "EPSG:5071"
     )
     cenloc_gdf_wt = cenloc_gdf_wt.sjoin(
-        osmclip, how="inner", predicate="within"
+        boundary, how="inner", predicate="within"
     )
 
     cenloc_final = cenloc[
@@ -89,6 +96,12 @@ if __name__ == "__main__":
         help="The two-digit state code for the point data.",
         type=str,
     )
+    parser.add_argument(
+        "--buffer",
+        required=False,
+        help="The amount to buffer the input shapefile in meters.",
+        type=int,
+    )
     args = parser.parse_args()
 
-    create_destpoint(args.year, args.geography, args.state)
+    create_destpoint(args.year, args.geography, args.state, args.buffer)
