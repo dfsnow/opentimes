63ed0d48ae7e80139a714a2d96f888736f694aa3
Dan Snow <dan@sno.ws>
2024-10-14 19:05:30 -0400

    Move range split to utils

---
+++ data/src/utils/inventory.py
@@ -0,0 +1,35 @@
+import math
+
+def split_range(n: int, n_chunks: int = 256, min_chunk_size = 5) -> list[tuple]:
+    """
+    Splits a range of integers into smaller chunks.
+
+    Args:
+        n: The total number of elements in the range.
+        n_chunks: The maximum number of chunks. Defaults to 256.
+        min_chunk_size: The minimum size of each chunk. Defaults to 5.
+
+    Returns:
+        list[tuple]: A list of tuples, where each tuple represents
+            the start and end indices of a chunk.
+    """
+    chunk_ranges = []
+    if n > n_chunks * min_chunk_size:
+        chunk_size = math.ceil(n / n_chunks)
+        for i in range(n // chunk_size):
+            start = i * chunk_size
+            end = ((i + 1) * chunk_size) - 1
+            chunk_ranges.append((start, end))
+    else:
+        n_chunks_small = n // min_chunk_size
+        for i in range(n_chunks_small):
+            start = i * min_chunk_size
+            end = ((i + 1) * min_chunk_size) - 1
+            chunk_ranges.append((start, end))
+
+    if chunk_ranges[-1][1] < n:
+        start, _ = chunk_ranges[-1]
+        chunk_ranges[-1] = (start, n - 1)
+
+    return(chunk_ranges)
+
