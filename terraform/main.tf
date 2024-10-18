4fa9e2941d5e9f6fae5277e54be7ec8062005f01
Dan Snow <dan@sno.ws>
2024-10-18 18:48:48 -0400

    Initialize terraform/opentofu

---
+++ terraform/main.tf
@@ -0,0 +1,30 @@
+terraform {
+  required_providers {
+    cloudflare = {
+      source  = "cloudflare/cloudflare"
+      version = "~> 4.0"
+    }
+  }
+
+  required_version = ">= 1.6.6"
+
+  backend "s3" {
+    bucket = "opentimes-resources"
+    key    = "terraform/terraform.tfstate"
+
+    region                      = "auto"
+    skip_credentials_validation = true
+    skip_metadata_api_check     = true
+    skip_region_validation      = true
+    skip_requesting_account_id  = true
+    skip_s3_checksum            = true
+    use_path_style              = true
+    endpoint = "https://fcb279b22cfe4c98f903ad8f9e7ccbb2.r2.cloudflarestorage.com"
+
+    /* Set AWS_PROFILE to "cloudflare" before running commands */
+  }
+}
+
+provider "cloudflare" {
+  api_token = var.cloudflare_api_token
+}
