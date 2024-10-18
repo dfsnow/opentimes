80810d34bde416515868b045e61533fcd63f3eb7
Dan Snow <dan@sno.ws>
2024-10-18 23:08:55 +0000

    Add helper for DVC adding files

---
+++ data/eda/add_dvc_files.py
@@ -0,0 +1,11 @@
+import os
+import yaml
+
+params = yaml.safe_load(open('params.yaml'))
+
+year = '2024'
+states = params["input"]["state"]
+
+for state in states:
+    print("Processing state:", state)
+    os.system(f'dvc commit -f intermediate/network/year={year}/geography=state/state={state}/network.dat')
