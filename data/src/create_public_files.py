624fedace1971be466bae2fbfd689617a94963dd
Dan Snow <dan@sno.ws>
2024-10-18 17:10:35 -0400

    Add initial public file creation script

---
+++ data/src/create_public_files.py
@@ -0,0 +1,71 @@
+import argparse
+
+import yaml
+from utils.inventory import create_duckdb_connection
+
+
+def create_public_files(version: str, mode: str, year: str) -> None:
+    params = yaml.safe_load(open("params.yaml"))
+    con = create_duckdb_connection()
+
+    # Dictionary of datasets and their respective number of partition folders
+    datasets = {
+        "times": 7,
+        "points": 7,
+        "missing_pairs": 6,
+        "metadata": 6
+    }
+
+    # Dictionary of filetypes to write and their DuckDB write options
+    filetypes = {
+        "parquet": f"""
+            FORMAT 'parquet',
+            COMPRESSION '{params['output']['compression']['type']}',
+            COMPRESSION_LEVEL {params['output']['compression']['level']}
+        """,
+        "csv.gz": """
+            FORMAT 'csv',
+            COMPRESSION 'gzip'
+        """
+    }
+
+    for dataset in datasets.keys():
+        partitions = "/*" * datasets[dataset]
+        print("Querying dataset:", dataset)
+        con.sql(
+            f"""
+            CREATE TABLE {dataset} AS
+                SELECT *
+                FROM read_parquet(
+                    'r2://{params['s3']['data_bucket']}/{dataset}{partitions}/*.parquet',
+                    hive_partitioning = true,
+                    hive_types_autocast = 0
+                )
+                WHERE version = '{version}'
+                    AND mode = '{mode}'
+                    AND year = '{year}'
+            """
+        )
+
+        for filetype in filetypes.keys():
+            filename = f"{dataset}-{version}-{mode}-{year}.{filetype}"
+            print("Creating public file for:", filename)
+            con.sql(
+                f"""
+                COPY {dataset}
+                TO 'r2://{params['s3']['public_bucket']}/{dataset}/{version}/{mode}/{year}/{filename}'
+                ({filetypes[filetype]});
+                """
+            )
+
+    con.close()
+
+
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser()
+    parser.add_argument("--version", required=True, type=str)
+    parser.add_argument("--mode", required=True, type=str)
+    parser.add_argument("--year", required=True, type=str)
+    args = parser.parse_args()
+
+    create_public_files(args.version, args.mode, args.year)
--- data/src/utils/inventory.py
+++ data/src/utils/inventory.py
@@ -1,21 +1,63 @@
 import math
+import os
+import tempfile
 from pathlib import Path
 
+import duckdb
 import pandas as pd
+import yaml
+
+
+def create_duckdb_connection() -> duckdb.DuckDBPyConnection:
+    params = yaml.safe_load(open("params.yaml"))
+    os.environ["AWS_PROFILE"] = params["s3"]["profile"]
+
+    con = duckdb.connect(database=":memory:")
+    for ext in ["parquet", "httpfs", "aws"]:
+        con.install_extension(ext)
+        con.load_extension(ext)
+    con.execute(
+        f"""
+        CREATE SECRET (
+            TYPE R2,
+            PROVIDER CREDENTIAL_CHAIN,
+            ACCOUNT_ID '{params["s3"]["account_id"]}'
+        );
+        """
+    )
+
+    return con
 
 
 def split_file_to_str(file: str | Path, **kwargs) -> str:
+    """
+    Splits the contents of a Parquet file into index strings.
+
+    Args:
+        file: The path to the Parquet file.
+        **kwargs: Additional keyword arguments passed to the
+            split_range function.
+
+    Returns:
+        A string representation of the chunked ranges in
+            the format '["start-end", ...]'.
+    """
     origins_df = pd.read_parquet(file)
 
     chunk_idx = split_range(len(origins_df), **kwargs)
     zfill_size = len(str(chunk_idx[-1][1]))
-    chunk_str = [f"{str(start).zfill(zfill_size)}-{str(end).zfill(zfill_size)}" for start, end in chunk_idx]
+    chunk_str = [
+        f"{str(start).zfill(zfill_size)}-{str(end).zfill(zfill_size)}"
+        for start, end in chunk_idx
+    ]
     chunk_out = '["' + '", "'.join(chunk_str) + '"]'
 
     return chunk_out
 
 
-def split_range(n: int, n_chunks: int = 256, min_chunk_size: int = 5) -> list[tuple]:
+def split_range(
+    n: int, n_chunks: int = 256, min_chunk_size: int = 5
+) -> list[tuple]:
     """
     Splits a range of integers into smaller chunks.
 
