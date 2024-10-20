b76ed5ffff103e95d2200f31a33972118529b7af
Dan Snow <dan@sno.ws>
2024-10-20 17:13:50 -0400

    Cleanup public file creation script and workflow

--- data/src/create_public_files.py
+++ data/src/create_public_files.py
@@ -2,20 +2,24 @@ import argparse
 import re
 
 import yaml
+from utils.datasets import DATASET_DICT
 from utils.utils import create_duckdb_connection
 
 
-def create_public_files(version: str, mode: str, year: str, dataset: str) -> None:
+def create_public_files(
+    dataset: str, version: str, mode: str, year: str, geography: str
+) -> None:
     """
     Janky function to pull data from the S3 output bucket and repartition it
     into files that live on a public-facing bucket. Goal is to consolidate
     many small files into a deterministic set of Parquet and CSV files.
 
     Args:
-        version: Comma-separated version of the data.
-        mode: Comma-separated travel mode.
-        year: Comma-separated year of the data.
-        dataset: Comma-separated dataset name.
+        dataset: Dataset name, one of ['times', 'points', 'missing_pairs', 'metadata'].
+        version: Version of the data e.g., 0.0.1.
+        mode: Travel mode, one of ['walk', 'bicycle', 'car', 'transit'].
+        year: Year of the data.
+        year: Census geography of the data. See params.yaml for list.
 
     Returns:
         None
@@ -23,70 +27,72 @@ def create_public_files(version: str, mode: str, year: str, dataset: str) -> Non
     params = yaml.safe_load(open("params.yaml"))
     con = create_duckdb_connection()
 
-    # Dictionary of datasets and their respective number of partition folders
-    dataset_dict = {
-        "times": 6,
-        "points": 7,
-        "missing_pairs": 6,
-        "metadata": 6
-    }
+    # Check that the input strings are valid
+    datasets = list(DATASET_DICT[version].keys())
+    if dataset not in datasets:
+        raise ValueError(
+            f"Input datasets must be one of: {', '.join(datasets)}"
+        )
+    semver_pattern = re.compile(r"^\d+\.\d+\.\d+$")
+    if not semver_pattern.match(version):
+        raise ValueError(
+            "Input version must be in semver format (e.g., 0.0.1)"
+        )
+    if mode not in params["times"]["mode"]:
+        raise ValueError(
+            f"Input mode must be one of: {', '.join(params['times']['mode'])}"
+        )
+    if year not in params["input"]["year"]:
+        raise ValueError(
+            f"Input years must be one of: {', '.join(params['input']['year'])}"
+        )
+    geographies = params["input"]["census"]["geography"]["all"]
+    if geography not in geographies:
+        raise ValueError(
+            f"Input years must be one of: {', '.join(geographies)}"
+        )
 
-    # Split the comma-separated input strings and check that they're valid
-    semver_pattern = re.compile(r'^\d+\.\d+\.\d+$')
-    version_list = [v.strip() for v in version.split(',')]
-    mode_list = [m.strip() for m in mode.split(',')]
-    year_list = [y.strip() for y in year.split(',')]
-    dataset_list = [d.strip() for d in dataset.split(',')]
+    filename = f"{dataset}-{version}-{mode}-{year}-{geography}.parquet"
+    partitions = "/*" * DATASET_DICT[version][dataset]["partition_levels"]
+    print("Creating file:", filename)
 
-    if not all(semver_pattern.match(v) for v in version_list):
-        raise ValueError("All input versions must be in semver format (e.g., 0.0.1)")
-    if not all(m in params["times"]["mode"] for m in mode_list):
-        raise ValueError(f"Input modes must be one of: {', '.join(params['times']['mode'])}")
-    if not all(y in params["input"]["year"] for y in year_list):
-        raise ValueError(f"Input years must be one of: {', '.join(params["input"]["year"])}")
-    datasets = list(dataset_dict.keys())
-    if not all(d in datasets for d in dataset_list):
-        raise ValueError(f"Input datasets must be one of: {', '.join(datasets)}")
-
-    for v in version_list:
-        for m in mode_list:
-            for y in year_list:
-                for d in dataset_list:
-                    filename = f"{d}-{v}-{m}-{y}.parquet"
-                    partitions = "/*" * dataset_dict[d]
-                    print("Creating file:", filename)
-
-                    con.sql(
-                        f"""
-                        COPY (
-                            SELECT *
-                            FROM read_parquet(
-                                'r2://{params['s3']['data_bucket']}/{d}{partitions}/*.parquet',
-                                hive_partitioning = true,
-                                hive_types_autocast = 0
-                            )
-                            WHERE version = '{v}'
-                                AND mode = '{m}'
-                                AND year = '{y}'
-                        )
-                        TO 'r2://{params['s3']['public_bucket']}/{d}/version={v}/mode={m}/year={y}/{filename}'
-                        (
-                            FORMAT 'parquet',
-                            COMPRESSION '{params['output']['compression']['type']}',
-                            COMPRESSION_LEVEL {params['output']['compression']['level']}
-                        );
-                        """
-                    )
+    con.sql(
+        f"""
+        COPY (
+            SELECT
+                {', '.join(DATASET_DICT[version][dataset]['public_file_columns'])},
+                regexp_extract(filename, 'part-(\\d+-\\d+)\\.parquet', 1) AS chunk_id
+            FROM read_parquet(
+                'r2://{params['s3']['data_bucket']}/{dataset}{partitions}/*.parquet',
+                hive_partitioning = true,
+                hive_types_autocast = false,
+                filename = true
+            )
+            WHERE version = '{version}'
+                AND mode = '{mode}'
+                AND year = '{year}'
+                AND geography = '{geography}'
+            ORDER BY {', '.join(DATASET_DICT[version][dataset]['public_file_order_by'])}
+        )
+        TO 'r2://{params['s3']['public_bucket']}/{dataset}/version={version}/mode={mode}/year={year}/geography={geography}/{filename}'
+        (
+            FORMAT 'parquet',
+            COMPRESSION '{params['output']['compression']['type']}',
+            COMPRESSION_LEVEL {params['output']['compression']['level']}
+        );
+        """
+    )
 
     con.close()
 
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser()
+    parser.add_argument("--dataset", required=True, type=str)
     parser.add_argument("--version", required=True, type=str)
     parser.add_argument("--mode", required=True, type=str)
     parser.add_argument("--year", required=True, type=str)
-    parser.add_argument("--dataset", required=True, type=str)
+    parser.add_argument("--geography", required=True, type=str)
     args = parser.parse_args()
 
-    create_public_files(args.version, args.mode, args.year, args.dataset)
+    create_public_files(args.dataset, args.version, args.mode, args.year, args.geography)
