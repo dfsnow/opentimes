cc4bdb16e1415e8faf9428e09b46fbf78c03f8cf
Dan Snow <dan@sno.ws>
2024-10-21 20:37:27 -0400

    Add example of opening pointer DB from URL

--- data/eda/duck.R
+++ data/eda/duck.R
@@ -1,82 +1,30 @@
 library(DBI)
 library(duckdb)
 
+# Example of loading pointer DB for URL
 con <- dbConnect(duckdb(), dbdir = ":memory:", read_only = FALSE)
-
 dbExecute(
   conn = con,
   "
   INSTALL httpfs;
   LOAD httpfs;
-  INSTALL aws;
-  LOAD aws;
-  CREATE SECRET (
-    TYPE R2,
-    PROVIDER CREDENTIAL_CHAIN,
-    ACCOUNT_ID 'fcb279b22cfe4c98f903ad8f9e7ccbb2'
-  );
+  ATTACH 'https://data.opentimes.org/databases/0.0.1.duckdb' AS opentimes;
   "
 )
 
-dbExecute(
-  conn = con,
-  "
-  CREATE OR REPLACE VIEW times AS
-  SELECT
-      version,
-      mode,
-      year,
-      geography,
-      state,
-      centroid_type,
-      origin_id,
-      destination_id,
-      time_min
-  FROM read_parquet('r2://opentimes-data/times/*/*/*/*/*/*/*/*.parquet', hive_partitioning = true, hive_types_autocast = 0)
-  "
-)
-
-tictoc::tic()
 ttm <- dbGetQuery(
   conn = con,
   "
   SELECT *
-  FROM times
+  FROM opentimes.times
   WHERE version = '0.0.1'
   AND mode = 'car'
-  AND year = '2020'
-  AND geography = 'county'
-  AND state = '01'
-  AND centroid_type = 'weighted'
-  AND origin_id = '01019'
-  "
-)
-tictoc::toc()
-
-dbExecute(
-  conn = con,
-  "
-  COPY (
-    SELECT
-      geography,
-      state,
-      centroid_type,
-      origin_id,
-      destination_id,
-      time_min
-    FROM times
-    WHERE version = '0.0.1'
-  )
-    TO 'r2://opentimes-public/version=0.0.1/mode=car/year=2020/test.parquet'
-    (FORMAT 'parquet', CODEC 'zstd', COMPRESSION_LEVEL 9);
+  AND year = '2024'
+  AND origin_id = '02020000101' 
   "
 )
 
-ttm <- dbGetQuery(
+dbGetQuery(
   conn = con,
-  "
-  SELECT *
-  FROM read_parquet('https://data.opentimes.org/points/0.0.1/car/2020/points-0.0.1-car-2020.parquet')
-  "
+  "SUMMARIZE opentimes.times"
 )
-# 1.285 seconds is the time to beat
