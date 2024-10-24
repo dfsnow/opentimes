4be60db80fa96ff9eec4d9f11e31798a0df06bdd
Dan Snow <dan@sno.ws>
2024-10-24 17:30:28 -0400

    Add initial multi-thread setup

--- data/src/calculate_times.py
+++ data/src/calculate_times.py
@@ -1,19 +1,109 @@
 import argparse
 import json
+import os
+import time
 from pathlib import Path
-from multiprocessing import Pool
+from concurrent.futures import ThreadPoolExecutor, as_completed
 
 import boto3
 import pandas as pd
-import time
-import valhalla  # pyright: ignore [reportMissingImports]
+import valhalla
 import yaml
 from utils.utils import format_time
 
-# Load parameters from file
-params = yaml.safe_load(open("params.yaml"))
+# Path within the valhalla-run Docker container that input/output directories
+# are mounted to
+DOCKER_PATH = Path("/data")
+
+# Load parameters from file and instantiate S3 connection
+params = yaml.safe_load(open(DOCKER_PATH / "params.yaml"))
+max_chunk_size = params["times"]["max_chunk_size"]
 session = boto3.Session(profile_name=params["s3"]["profile"])
 s3 = session.client("s3", endpoint_url=params["s3"]["endpoint"])
+
+
+def calculate_times(
+    actor,
+    o_start_idx: int,
+    d_start_idx: int,
+    origins: pd.DataFrame,
+    destinations: pd.DataFrame,
+    max_chunk_size: int,
+    mode: str,
+) -> pd.DataFrame:
+    """Calculates travel times and distances between origins and destinations.
+
+    Args:
+        actor: Valhalla actor instance for making matrix API requests.
+        o_start_idx: Starting index for the origins DataFrame.
+        d_start_idx: Starting index for the destinations DataFrame.
+        origins: DataFrame containing origin points with 'lat' and 'lon' columns.
+        destinations: DataFrame containing destination points with 'lat' and 'lon' columns.
+        max_chunk_size: Maximum number of points to process in one chunk.
+        mode: Travel mode for the Valhalla API (e.g., 'auto', 'bicycle').
+
+    Returns:
+        DataFrame containing origin IDs, destination IDs, travel durations, and distances.
+    """
+    start_time = time.time()
+    o_end_idx = min(o_start_idx + max_chunk_size, len(origins))
+    d_end_idx = min(d_start_idx + max_chunk_size, len(destinations))
+    job_string = (
+        f"Starting origins {o_start_idx}-{o_end_idx} and "
+        f"destinations {d_start_idx}-{d_end_idx}"
+    )
+    print(job_string)
+
+    # Get the subset of origin and destination points and convert them to lists
+    # then squash them into the request body
+    origins_list = (
+        origins.iloc[o_start_idx:o_end_idx]
+        .apply(lambda row: {"lat": row["lat"], "lon": row["lon"]}, axis=1)
+        .tolist()
+    )
+    destinations_list = (
+        destinations.iloc[d_start_idx:d_end_idx]
+        .apply(lambda row: {"lat": row["lat"], "lon": row["lon"]}, axis=1)
+        .tolist()
+    )
+    request_json = json.dumps(
+        {
+            "sources": origins_list,
+            "targets": destinations_list,
+            "costing": mode,
+            "verbose": False,
+        }
+    )
+
+    # Make the actual request to the matrix API
+    response = actor.matrix(request_json)
+    response_data = json.loads(response)
+
+    # Parse the response data and convert it to a dataframe. Recover the
+    # origin and destination indices and append them to the dataframe
+    durations = response_data["sources_to_targets"]["durations"]
+    distances = response_data["sources_to_targets"]["distances"]
+    origin_ids = (
+        origins.iloc[o_start_idx:o_end_idx]["id"]
+        .repeat(d_end_idx - d_start_idx)
+        .tolist()
+    )
+    destination_ids = destinations.iloc[d_start_idx:d_end_idx][
+        "id"
+    ].tolist() * (o_end_idx - o_start_idx)
+
+    df = pd.DataFrame(
+        {
+            "origin_id": origin_ids,
+            "destination_id": destination_ids,
+            "duration_sec": [i for sl in durations for i in sl],
+            "distance_km": [i for sl in distances for i in sl],
+        }
+    )
+
+    elapsed_time = time.time() - start_time
+    print(job_string, f": {format_time(elapsed_time)}")
+    return df
 
 
 if __name__ == "__main__":
@@ -47,24 +137,23 @@ if __name__ == "__main__":
     input = {}
     input["main"] = {
         "path": Path(
-            (
-                f"year={args.year}/geography={args.geography}/"
-                f"state={args.state}/{args.state}.parquet"
-            )
+            f"year={args.year}/geography={args.geography}/"
+            f"state={args.state}/{args.state}.parquet",
         )
     }
     input["dirs"] = {
         "valhalla_tiles": Path(
-            (
-                f"intermediate/valhalla_tiles/year={args.year}/"
-                f"geography=state/state={args.state}"
-            )
+            DOCKER_PATH,
+            f"intermediate/valhalla_tiles/year={args.year}/"
+            f"geography=state/state={args.state}",
         )
     }
     input["files"] = {
-        "origins_file": Path(f"intermediate/cenloc/{input['main']['path']}"),
+        "origins_file": Path(
+            DOCKER_PATH, f"intermediate/cenloc/{input['main']['path']}"
+        ),
         "destinations_file": Path(
-            f"intermediate/destpoint/{input['main']['path']}"
+            DOCKER_PATH, f"intermediate/destpoint/{input['main']['path']}"
         ),
     }
 
@@ -72,7 +161,7 @@ if __name__ == "__main__":
     if args.write_to_s3:
         output_prefix = params["s3"]["data_bucket"]
     else:
-        output_prefix = "output"
+        output_prefix = DOCKER_PATH / "output"
 
     output = {}
     output["main"] = {
@@ -124,8 +213,9 @@ if __name__ == "__main__":
     }
 
     # Make sure outputs have somewhere to write to
-    for dir_path in output["dirs"].values():
-        dir_path.mkdir(parents=True, exist_ok=True)
+    if not args.write_to_s3:
+        for path in output["files"].values():
+            path.mkdir(parents=True, exist_ok=True)
 
     ##### DATA PREP #####
 
@@ -154,51 +244,29 @@ if __name__ == "__main__":
 
     # Initialize the Valhalla actor bindings
     actor = valhalla.Actor((Path.cwd() / "valhalla.json").as_posix())
+    num_cores = os.cpu_count() or 1
+    num_cores = max(1, num_cores - 1)
+    print("Starting ThreadPoolExecutor with", num_cores, "cores")
 
-    # Use the Valhalla Python bindings to query the Valhalla matrix API, then
-    # store the JSON results
-    # responses = []
-    for o in range(0, n_origins, params["times"]["max_chunk_size"]):
-        for d in range(0, n_destinations, params["times"]["max_chunk_size"]):
-            start_time = time.time()
-            o_idx = min(o + params["times"]["max_chunk_size"], n_origins)
-            d_idx = min(d + params["times"]["max_chunk_size"], n_destinations)
-            print(
-                "Calculating times for origins",
-                f"{o}-{o_idx} and destinations {d}-{d_idx}",
+    with ThreadPoolExecutor(num_cores) as executor:
+        futures = [
+            executor.submit(
+                calculate_times,
+                actor=actor,
+                o_start_idx=o,
+                d_start_idx=d,
+                origins=origins,
+                destinations=destinations,
+                max_chunk_size=max_chunk_size,
+                mode=args.mode,
             )
+            for o in range(0, n_origins, max_chunk_size)
+            for d in range(0, n_destinations, max_chunk_size)
+        ]
 
-            origins_list = (
-                origins.iloc[o:o_idx]
-                .apply(
-                    lambda row: {"lat": row["lat"], "lon": row["lon"]}, axis=1
-                )
-                .tolist()
-            )
-            destinations_list = (
-                destinations.iloc[d:d_idx]
-                .apply(
-                    lambda row: {"lat": row["lat"], "lon": row["lon"]}, axis=1
-                )
-                .tolist()
-            )
-            request_json = json.dumps(
-                {
-                    "sources": origins_list,
-                    "targets": destinations_list,
-                    "costing": args.mode,
-                    "verbose": False
-                }
-            )
-            out = actor.matrix(request_json)
+        results = []
+        for future in as_completed(futures):
+            results.append(future.result())
 
-            data = json.loads(out)
-            with open('output.json', 'w') as json_file:
-                json.dump(data, json_file, indent=2)
-
-            elapsed_time = time.time() - start_time
-            print(f"Iteration time: {format_time(elapsed_time)}")
-
-    breakpoint()
 
     # df.to_parquet('s3://your-bucket-name/path/to/file.parquet', storage_options={'client': boto3.client('s3')})
