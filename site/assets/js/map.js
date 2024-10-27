d527ab70d7e441c54f7c0cb09324b96b1a27bf91
Dan Snow <dan@sno.ws>
2024-10-26 22:28:57 -0400

    Add initial protomaps setup

--- site/assets/js/map.js
+++ site/assets/js/map.js
@@ -1,6 +1,19 @@
+let protocol = new pmtiles.Protocol();
+maplibregl.addProtocol("pmtiles", protocol.tile);
+
 const map = new maplibregl.Map({
-  style: 'https://tiles.openfreemap.org/styles/positron',
+  style: "https://tiles.openfreemap.org/styles/positron",
   center: [-74.0, 40.75],
   zoom: 10,
-  container: 'map',
+  pitchWithRotate: false,
+  container: "map",
+})
+
+map.on("load", async () => {
+  map.addSource("protomap", {
+    type: "vector",
+    url: "pmtiles://https://data.opentimes.org/tiles/tracts_2024.pmtiles"
+  });
+  map.addControl(new maplibregl.NavigationControl());
+  map.addControl(new maplibregl.ScaleControl({ unit: "metric" }));
 })
