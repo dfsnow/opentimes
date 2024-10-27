a838f6162fc23270a0cb0e94c6b0ae1bb6351bf8
Dan Snow <dan@sno.ws>
2024-10-27 02:51:29 -0400

    Add working isochrone

--- site/assets/js/map.js
+++ site/assets/js/map.js
@@ -37,7 +37,8 @@ const map = new maplibregl.Map({
 map.on("load", async () => {
   map.addSource("protomap", {
     type: "vector",
-    url: "pmtiles://https://data.opentimes.org/tiles/tracts_2024.pmtiles"
+    url: "pmtiles://https://data.opentimes.org/tiles/tracts_2024.pmtiles",
+    promoteId: "id"
   });
   map.addControl(new maplibregl.NavigationControl());
   map.addLayer({
@@ -47,7 +48,21 @@ map.on("load", async () => {
     "source-layer": "tracts",
     filter: ["==", ["geometry-type"], "Polygon"],
     "paint": {
-      "fill-opacity": 0.0
+      "fill-opacity": 0.5,
+      "fill-color": [
+        "case",
+        ["==", ["feature-state", "tract_color"], "color_1"], "#FDE725",
+        ["==", ["feature-state", "tract_color"], "color_2"], "#B4DE2C",
+        ["==", ["feature-state", "tract_color"], "color_3"], "#6DCD59",
+        ["==", ["feature-state", "tract_color"], "color_4"], "#35B779",
+        ["==", ["feature-state", "tract_color"], "color_5"], "#1F9E89",
+        ["==", ["feature-state", "tract_color"], "color_6"], "#26828E",
+        ["==", ["feature-state", "tract_color"], "color_7"], "#31688E",
+        ["==", ["feature-state", "tract_color"], "color_8"], "#3E4A89",
+        ["==", ["feature-state", "tract_color"], "color_9"], "#482878",
+        ["==", ["feature-state", "tract_color"], "color_10"], "#440154",
+        "#ffffff" // Default color if no match found
+      ]
     },
   });
   map.addLayer({
@@ -58,7 +73,7 @@ map.on("load", async () => {
     filter: ["==", ["geometry-type"], "Polygon"],
     "paint": {
       "line-opacity": 0.2,
-      "line-color": "#000",
+      "line-color": "#333",
     },
   });
 
@@ -97,6 +112,7 @@ map.on("load", async () => {
   const db = await DuckDB.connect();
   db.query(`ATTACH 'https://data.opentimes.org/databases/0.0.1.duckdb' AS opentimes (READ_ONLY);`);
 
+  let previousStates = [];
   map.on("click", async (e) => {
     const features = map.queryRenderedFeatures(e.point, {
       layers: ["tracts_fill"]
@@ -104,6 +120,7 @@ map.on("load", async () => {
 
     if (features.length > 0) {
       const feature = features[0];
+      const state = feature.properties.state;
       const id = feature.properties.id;
       console.log(id);
 
@@ -115,9 +132,39 @@ map.on("load", async () => {
             AND year = '2024'
             AND geography = 'tract'
             AND centroid_type = 'weighted'
-            AND origin_id = ${id}
+            AND state = '${state}'
+            AND origin_id = '${id}'
       `);
-      console.log(result);
+
+      const colorScale = (duration) => {
+        if (duration < 1800) return "color_1";
+        if (duration < 3600) return "color_2";
+        if (duration < 5400) return "color_3";
+        if (duration < 7200) return "color_4";
+        if (duration < 9000) return "color_5";
+        if (duration < 10800) return "color_6";
+        if (duration < 12600) return "color_7";
+        if (duration < 14400) return "color_8";
+        if (duration < 16200) return "color_9";
+        return "color_10";
+      };
+
+      // Clear previous feature states
+      previousStates.forEach(state => {
+        map.setFeatureState(
+          { source: "protomap", sourceLayer: "tracts", id: state.id },
+          { tract_color: "none" }
+        );
+      });
+
+      previousStates = result.toArray().map(row => {
+        const tmp = row.toJSON();
+        map.setFeatureState(
+          { source: "protomap", sourceLayer: "tracts", id: tmp.destination_id },
+          { tract_color: colorScale(tmp.duration_sec) }
+        );
+        return { id: tmp.destination_id };
+      });
     }
   });
 })
