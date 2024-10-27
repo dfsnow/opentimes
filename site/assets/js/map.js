32514df1970cb28e492a695e3aa9eba9c1d18489
Dan Snow <dan@sno.ws>
2024-10-26 23:52:29 -0400

    Add working tract ID popup

--- site/assets/js/map.js
+++ site/assets/js/map.js
@@ -6,6 +6,7 @@ const map = new maplibregl.Map({
   center: [-74.0, 40.75],
   zoom: 10,
   pitchWithRotate: false,
+  doubleClickZoom: false,
   container: "map",
   maxZoom: 12
 })
@@ -16,9 +17,19 @@ map.on("load", async () => {
     url: "pmtiles://https://data.opentimes.org/tiles/tracts_2024.pmtiles"
   });
   map.addControl(new maplibregl.NavigationControl());
-  map.addControl(new maplibregl.ScaleControl({ unit: "metric" }));
+  map.addControl(new maplibregl.ScaleControl({ unit: "metric", position: 'bottom-left' }));
   map.addLayer({
-    "id": "tracts",
+    "id": "tracts_fill",
+    "type": "fill",
+    "source": "protomap",
+    "source-layer": "tracts",
+    filter: ["==", ["geometry-type"], "Polygon"],
+    "paint": {
+      "fill-opacity": 0.0
+    },
+  });
+  map.addLayer({
+    "id": "tracts_line",
     "type": "line",
     "source": "protomap",
     "source-layer": "tracts",
@@ -27,5 +38,35 @@ map.on("load", async () => {
       "line-opacity": 0.2,
       "line-color": "#000",
     },
+  });
+
+  // Create a display element for the tract ID
+  const tractIdDisplay = document.createElement("div");
+  tractIdDisplay.style.position = "absolute";
+  tractIdDisplay.style.bottom = "40px";
+  tractIdDisplay.style.left = "10px";
+  tractIdDisplay.style.backgroundColor = "rgba(255, 255, 255, 0.75)";
+  tractIdDisplay.style.padding = "5px";
+  tractIdDisplay.style.borderRadius = "5px";
+  tractIdDisplay.style.fontFamily = "Arial, sans-serif";
+  tractIdDisplay.style.fontSize = "2rem";
+  tractIdDisplay.style.pointerEvents = "none";
+  document.body.appendChild(tractIdDisplay);
+
+  map.on("mousemove", (e) => {
+    const features = map.queryRenderedFeatures(e.point, {
+      layers: ["tracts_fill"]
+    });
+
+    if (features.length > 0) {
+      const feature = features[0];
+      const id = feature.properties.id;
+      const coordinates = feature.geometry.coordinates[0][0];
+      map.getCanvas().style.cursor = "pointer";
+      tractIdDisplay.textContent = `Tract ID: ${id}`;
+    } else {
+      map.getCanvas().style.cursor = "";
+      tractIdDisplay.textContent = "";
+    }
   });
 })
