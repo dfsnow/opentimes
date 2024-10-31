ddd356c5e015fda95a3cace1713f308922fdb4db
Dan Snow <dan@sno.ws>
2024-10-31 03:52:54 -0400

    Add pre-commit workflow and config

--- data/eda/add_dvc_files.py
+++ data/eda/add_dvc_files.py
@@ -1,11 +1,13 @@
 import os
 import yaml
 
-params = yaml.safe_load(open('params.yaml'))
+params = yaml.safe_load(open("params.yaml"))
 
-year = '2024'
+year = "2024"
 states = params["input"]["state"]
 
 for state in states:
     print("Processing state:", state)
-    os.system(f'dvc commit -f intermediate/network/year={year}/geography=state/state={state}/network.dat')
+    os.system(
+        f"dvc commit -f intermediate/network/year={year}/geography=state/state={state}/network.dat"
+    )
