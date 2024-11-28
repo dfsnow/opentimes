29f077c75f8c5f96bf3fc7ef3e4f6f3365e30f74
Dan Snow <dan@sno.ws>
2024-11-27 20:58:38 -0600

    Add logging and cleanup fetch scripts

--- data/src/fetch_blockpop.py
+++ data/src/fetch_blockpop.py
@@ -4,6 +4,9 @@ from pathlib import Path
 
 import pandas as pd
 import requests as r
+from utils.logging import create_logger
+
+logger = create_logger(__name__)
 
 BASE_URL = "https://api.census.gov/data/"
 CENSUS_API_KEY = os.getenv("CENSUS_API_KEY")
@@ -17,18 +20,13 @@ def fetch_blockpop(year: str, state: str) -> None:
     Args:
         year: The year of the decennial Census.
         state: The two-digit state FIPS code.
-
-    Returns:
-        None
     """
     output_dir = (
         Path.cwd() / "input" / "blockpop" / f"year={year}" / f"state={state}"
     )
     output_file = output_dir / f"{state}.parquet"
-
     output_dir.mkdir(parents=True, exist_ok=True)
 
-    # Construct the URL to fetch block population data
     pop_var = "P001001" if year == "2010" else "P1_001N"
     url = BASE_URL + (
         f"{year}/dec/pl?get={pop_var}&for=block:*&in=state:"
@@ -39,6 +37,11 @@ def fetch_blockpop(year: str, state: str) -> None:
         response = r.get(url, stream=True)
         response.raise_for_status()
 
+        if "Invalid Key" in response.text:
+            msg = "Invalid Census API key provided"
+            logger.error(msg)
+            raise ValueError(msg)
+
         data = pd.DataFrame(
             response.json()[1:],
             columns=["population", "state", "county", "tract", "block"],
@@ -48,17 +51,20 @@ def fetch_blockpop(year: str, state: str) -> None:
         data.drop(columns=["state"], inplace=True)
         data.to_parquet(output_file, engine="pyarrow", index=False)
 
-        print(f"File downloaded successfully: {output_file}")
+        logger.info(f"File downloaded successfully: {output_file}")
     except r.exceptions.RequestException as e:
-        print(f"Failed to download file: {e}")
+        logger.error(f"Failed to download file: {e}")
 
 
-if __name__ == "__main__":
+def main() -> None:
     parser = argparse.ArgumentParser()
     parser.add_argument(
         "--year", required=True, choices=["2010", "2020"], type=str
     )
     parser.add_argument("--state", required=False, type=str)
     args = parser.parse_args()
-
     fetch_blockpop(args.year, args.state)
+
+
+if __name__ == "__main__":
+    main()
--- data/src/fetch_tiger.py
+++ data/src/fetch_tiger.py
@@ -2,6 +2,9 @@ import argparse
 from pathlib import Path
 
 import requests as r
+from utils.logging import create_logger
+
+logger = create_logger(__name__)
 
 # Base URL for TIGER/Line shapefiles
 BASE_URL = "https://www2.census.gov/geo/tiger/TIGER"
@@ -24,39 +27,31 @@ def fetch_shapefile(
     year: str, geography: str, state: str | None = None
 ) -> None:
     """
-    Fetches a TIGER/Line shapefile and saves it to a directory partitioned by
-    year, geography, and (optionally) state.
+    Fetches a TIGER/Line shapefile and saves it to a directory.
+
+    The output directory is partitioned by year, geography, and optionally state.
 
     Args:
         year: The year of the TIGER/Line data.
         geography: The geography type of the shapefile.
         state: The two-digit state FIPS code for the shapefile.
-
-    Returns:
-        None
     """
-    output_dir = (
+    base_dir = (
         Path.cwd()
         / "input"
         / "tiger"
         / f"year={year}"
         / f"geography={geography}"
     )
-    if not state:
-        output_file = output_dir / f"{geography}.zip"
-    else:
-        output_dir = output_dir / f"state={state}"
-        output_file = output_dir / f"{state}.zip"
-
+    output_dir = base_dir / f"state={state}" if state else base_dir
+    output_file = output_dir / f"{state if state else geography}.zip"
     output_dir.mkdir(parents=True, exist_ok=True)
 
-    # Construct the URL to fetch TIGER data
     tiger_geo_name = TIGER_GEO_NAMES[geography]
-    if not state:
-        remote_file_name = f"tl_{year}_us_{tiger_geo_name}.zip"
-    else:
-        remote_file_name = f"tl_{year}_{state}_{tiger_geo_name}.zip"
+    file_prefix = f"tl_{year}_{'us' if not state else state}"
+    remote_file_name = f"{file_prefix}_{tiger_geo_name}.zip"
     url = f"{BASE_URL}{year}/{tiger_geo_name.upper()}/{remote_file_name}"
+
     try:
         response = r.get(url, stream=True)
         response.raise_for_status()
@@ -64,13 +59,13 @@ def fetch_shapefile(
         with open(output_file, "wb") as file:
             for chunk in response.iter_content(chunk_size=8192):
                 file.write(chunk)
+        logger.info(f"File downloaded successfully: {output_file}")
 
-        print(f"File downloaded successfully: {output_file}")
     except r.exceptions.RequestException as e:
-        print(f"Failed to download file: {e}")
+        logger.error(f"Failed to download file: {e}")
 
 
-if __name__ == "__main__":
+def main() -> None:
     parser = argparse.ArgumentParser()
     parser.add_argument("--year", required=True, type=str)
     parser.add_argument("--geography", required=True, type=str)
@@ -78,3 +73,7 @@ if __name__ == "__main__":
     args = parser.parse_args()
 
     fetch_shapefile(year=args.year, geography=args.geography, state=args.state)
+
+
+if __name__ == "__main__":
+    main()
