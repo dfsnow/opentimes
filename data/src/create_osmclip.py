4173a6fbd26e75bf05ee2a1c9dbe9f8d9847cf74
Dan Snow <dan@sno.ws>
2024-11-27 21:53:01 -0600

    Add logging to osmclip

--- data/src/create_osmclip.py
+++ data/src/create_osmclip.py
@@ -2,20 +2,20 @@ import argparse
 from pathlib import Path
 
 from utils.census import load_shapefile
+from utils.logging import create_logger
+
+logger = create_logger(__name__)
 
 
 def create_osmclip(year: str, state: str, buffer: int = 0) -> None:
     """
-    Converts TIGER/Line shapefile to a buffered GeoJSON used as a clipping
+    Convert a TIGER/Line shapefile to a buffered GeoJSON used as a clipping
     boundary for the OpenStreetMap road network.
 
     Args:
         year: The year of the TIGER/Line data.
         state: The two-digit state FIPS code for the shapefile.
         buffer: The amount to buffer the input shapefile (in meters).
-
-    Returns:
-        None
     """
     tiger_file = (
         Path.cwd()
@@ -34,24 +34,29 @@ def create_osmclip(year: str, state: str, buffer: int = 0) -> None:
         / f"state={state}"
     )
     output_file = output_dir / f"{state}.geojson"
-
     output_dir.mkdir(parents=True, exist_ok=True)
 
     gdf = load_shapefile(tiger_file)
     gdf = gdf[gdf["geoid"] == state]
     gdf = gdf[["geometry"]]
+    logger.info(f"Loaded state boundary for {state}")
     if buffer:
         gdf.to_crs(crs="EPSG:5071", inplace=True)
         gdf["geometry"] = gdf["geometry"].buffer(distance=buffer)
+        logger.info(f"Buffered state boundary by {buffer} meters")
     gdf.to_crs(crs="EPSG:4326", inplace=True)
     gdf.to_file(output_file, driver="GeoJSON")
+    logger.info(f"Wrote to: {output_file}")
 
 
-if __name__ == "__main__":
+def main() -> None:
     parser = argparse.ArgumentParser()
     parser.add_argument("--year", required=True, type=str)
     parser.add_argument("--state", required=True, type=str)
     parser.add_argument("--buffer", required=False, type=int)
     args = parser.parse_args()
-
     create_osmclip(args.year, args.state, args.buffer)
+
+
+if __name__ == "__main__":
+    main()
