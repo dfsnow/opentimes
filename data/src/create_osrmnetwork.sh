aef4df382a223ca6f6ade3d992d68c3ac1e56d6d
Dan Snow <dan@sno.ws>
2025-01-26 20:38:07 -0600

    Remove containers on job completion

--- data/src/create_osrmnetwork.sh
+++ data/src/create_osrmnetwork.sh
@@ -17,9 +17,9 @@ rm -rf ./"$BUILD_DIR"/*
 mkdir -p ./"$OUT_DIR"
 ln ./"$IN_FILE" ./"$BUILD_DIR"/"$state".osm.pbf
 
-docker run -t -v ./"$BUILD_DIR":/data osrm/osrm-backend \
+docker run --rm -t -v ./"$BUILD_DIR":/data osrm/osrm-backend \
     osrm-extract -p /opt/"$mode".lua /data/"$state".osm.pbf
-docker run -t -v ./"$BUILD_DIR":/data osrm/osrm-backend \
+docker run --rm -t -v ./"$BUILD_DIR":/data osrm/osrm-backend \
     osrm-contract /data/"$state".osrm
 
 rm ./"$BUILD_DIR"/"$state".osm.pbf
