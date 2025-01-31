2fa0c24acf31d42a2de5659ad5341881ad367345
Dan Snow <dan@sno.ws>
2025-01-30 22:35:21 -0600

    Refactor script to make public files

--- data/src/create_public_files.py
+++ data/src/create_public_files.py
@@ -15,6 +15,7 @@ def create_public_files(
     mode: str,
     year: str,
     geography: str,
+    state: str,
 ) -> None:
     """
     Janky function to pull data from the S3 output bucket and repartition it
@@ -27,12 +28,11 @@ def create_public_files(
         mode: Travel mode, one of ['walk', 'bicycle', 'car', 'transit'].
         year: Year of the data.
         geography: Census geography of the data. See params.yaml for list.
+        state: State of the data.
     """
     with open("params.yaml") as file:
         params = yaml.safe_load(file)
-    states = params["input"]["state"]
     con = create_duckdb_connection()
-    logger.info("Successfully connected to DuckDB")
 
     # Check that the input strings are valid
     datasets = list(DATASET_DICT[version].keys())
@@ -59,41 +59,40 @@ def create_public_files(
             f"Input geography must be one of: {', '.join(geographies)}"
         )
 
-    for state in states:
-        filename = f"{dataset}-{version}-{mode}-{year}-{geography}-{state}"
-        partitions = "/*" * DATASET_DICT[version][dataset]["partition_levels"]
+    filename = f"{dataset}-{version}-{mode}-{year}-{geography}-{state}"
+    partitions = "/*" * DATASET_DICT[version][dataset]["partition_levels"]
 
-        con.sql(
-            f"""
-            COPY (
-                SELECT
-                    {', '.join(DATASET_DICT[version][dataset]['public_file_columns'])},
-                    regexp_extract(filename, 'part-(\\d+-\\d+)\\.parquet', 1) AS chunk_id
-                FROM read_parquet(
-                    'r2://{params['s3']['data_bucket']}/{dataset}{partitions}/*.parquet',
-                    hive_partitioning = true,
-                    hive_types_autocast = false,
-                    filename = true
-                )
-                WHERE version = '{version}'
-                    AND mode = '{mode}'
-                    AND year = '{year}'
-                    AND geography = '{geography}'
-                    AND state = '{state}'
-                ORDER BY {', '.join(DATASET_DICT[version][dataset]['order_by_columns'])}
+    con.sql(
+        f"""
+        COPY (
+            SELECT
+                {", ".join(DATASET_DICT[version][dataset]["public_file_columns"])},
+                regexp_extract(filename, 'part-(\\d+-\\d+)\\.parquet', 1) AS chunk_id
+            FROM read_parquet(
+                'r2://{params["s3"]["data_bucket"]}/{dataset}{partitions}/*.parquet',
+                hive_partitioning = true,
+                hive_types_autocast = false,
+                filename = true
             )
-            TO 'r2://{params['s3']['public_bucket']}/{dataset}/version={version}/mode={mode}/year={year}/geography={geography}/state={state}'
-            (
-                FORMAT 'parquet',
-                COMPRESSION '{params['output']['compression']['type']}',
-                COMPRESSION_LEVEL {params['output']['compression']['level']},
-                OVERWRITE_OR_IGNORE true,
-                FILENAME_PATTERN '{filename}-',
-                FILE_SIZE_BYTES 475000000
-            );
-            """
+            WHERE version = '{version}'
+                AND mode = '{mode}'
+                AND year = '{year}'
+                AND geography = '{geography}'
+                AND state = '{state}'
+            ORDER BY {", ".join(DATASET_DICT[version][dataset]["order_by_columns"])}
         )
-        logger.info(f"Created file: {filename}")
+        TO 'r2://{params["s3"]["public_bucket"]}/{dataset}/version={version}/mode={mode}/year={year}/geography={geography}/state={state}'
+        (
+            FORMAT 'parquet',
+            COMPRESSION '{params["output"]["compression"]["type"]}',
+            COMPRESSION_LEVEL {params["output"]["compression"]["level"]},
+            OVERWRITE_OR_IGNORE true,
+            FILENAME_PATTERN '{filename}-',
+            FILE_SIZE_BYTES 475000000
+        );
+        """
+    )
+    logger.info(f"Created file: {filename}")
 
     con.close()
 
@@ -105,9 +104,15 @@ def main() -> None:
     parser.add_argument("--mode", required=True, type=str)
     parser.add_argument("--year", required=True, type=str)
     parser.add_argument("--geography", required=True, type=str)
+    parser.add_argument("--state", required=True, type=str)
     args = parser.parse_args()
     create_public_files(
-        args.dataset, args.version, args.mode, args.year, args.geography
+        args.dataset,
+        args.version,
+        args.mode,
+        args.year,
+        args.geography,
+        args.state,
     )
 
 
